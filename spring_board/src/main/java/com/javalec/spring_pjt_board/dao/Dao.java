69564821eec68720ea9f0afb4ae53fe51076196a
LEESUNGWOOaas <sa775628@gmail.com>
2020-11-03 15:47:24 +0900

    Dao의 동작코드 작성

--- spring_board/src/main/java/com/javalec/spring_pjt_board/dao/Dao.java
+++ spring_board/src/main/java/com/javalec/spring_pjt_board/dao/Dao.java
@@ -1,14 +1,76 @@
 package com.javalec.spring_pjt_board.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 import com.javalec.spring_pjt_board.dto.Dto;
 
 public class Dao {
 
+	DataSource dataSource;
+	
+	public Dao() {
+		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/orcl");
+		} catch (NamingException e) {			
+			e.printStackTrace();
+		}
+		
+	}
 	public ArrayList<Dto> list() {
 		
-		ArrayList<Dto> dtos= null;
+		ArrayList<Dto> dtos= new ArrayList<Dto>();//배열 ArrayList로 만듬
+		Connection connection = null;//DBCP를 이용한 커넥션 객체 생성 
+		PreparedStatement preparedStatement = null;//변수를 대입하여 쿼리를 수행하는 방식을 사용
+		ResultSet resultSet = null;//커맨드창이 아닌상태에서 자바로 조회 커리문 사용하는 객체 사용 
+		
+		try {
+			connection = dataSource.getConnection();//커넥션 객체를 구함
+			String query = "select bId,bName,bTitle,bContent,bDate,bHit,bGroup,bStep,bIntent from mvc_board order by bGroup desc,bStep asc";
+			preparedStatement = connection.prepareStatement(query);
+			resultSet = preparedStatement.executeQuery();
+			
+			//쿼리문을 resultSet을 분리해야됌
+		 while(resultSet.next()) {
+			 int bId = resultSet.getInt("bId");
+			 String bName = resultSet.getString("bName");
+			 String bTitle = resultSet.getString("bTitle");
+			 String bContent = resultSet.getString("bContent");
+			 Timestamp bDate = resultSet.getTimestamp("bDate");
+			 int bHit = resultSet.getInt("bHit");
+			 int bStep = resultSet.getInt("bStep");
+			 int bGroup = resultSet.getInt("bGroup");
+			 int bIntent = resultSet.getInt("bIntent");
+			 
+			 
+			 Dto dto = new Dto (bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIntent);
+			 dtos.add(dto);//while 반복문을 통해 계속 돌면서 레코드값을 계속 dto에 담는다
+		 }
+			 
+			 
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}finally{//자원 해제
+			try {
+			if(resultSet != null)resultSet.close();
+			if(preparedStatement != null)preparedStatement.close();
+			if(connection != null)connection.close();
+			}catch(Exception e2){
+				 
+			}
+			
+		}
 		
 		return dtos; //작업결과물을 dto로 넘겨줌 
 	}
--- spring_board/src/main/java/com/javalec/spring_pjt_board/dto/Dto.java
+++ spring_board/src/main/java/com/javalec/spring_pjt_board/dto/Dto.java
@@ -3,25 +3,29 @@ package com.javalec.spring_pjt_board.dto;
 import java.sql.Timestamp;
 
 public class Dto {
-	int bId;
-	String bName;
-	String bTitle;
-	Timestamp bDate;// date하위객체의 timestamp를 사용 (java.sql)
-	int bHit;
-	int bGroup;
-	int bIntent;
+	int bId;//게시판id
+	String bName;//작성자
+	String bTitle;//글제목
+	String bContent;//내용
+	Timestamp bDate;// Date하위객체의 timestamp를 사용 (java.sql)
+	int bHit;	//조회수
+	int bGroup;	//답글(리플)의 그룹번호
+	int bStep;	//글이 위에서 몇번쨰에 위치한지
+	int bIntent; // 들여쓰기 답글의 위치bit 차이를 줘서 글이 띄어써지는 것
 
 	public Dto() {// default 생성자
 
 	}
 
-	public Dto(int bId,String bName,String bTitle,Timestamp bDate,int bHit,int bGroup,int bIntent) {// 생성자를 통해 바로바로 값을 넣을수 있게 파라미터 받음
+	public Dto(int bId,String bName,String bTitle,String bContent,Timestamp bDate,int bHit,int bGroup,int bStep,int bIntent) {// 생성자를 통해 바로바로 값을 넣을수 있게 파라미터 받음
 		this.bId = bId;
 		this.bName = bName;
+		this.bContent = bContent;
 		this.bDate = bDate;
 		this.bTitle = bTitle;
 		this.bHit = bHit; 
 		this.bGroup = bGroup;
+		this.bStep = bStep;
 		this.bIntent = bIntent;
 	}
 
@@ -50,6 +54,14 @@ public class Dto {
 	}
 
 
+	public String getbContent() {
+		return bContent;
+	}
+
+	public void setbContent(String bContent) {
+		this.bContent = bContent;
+	}
+
 	public Timestamp getbDate() {
 		return bDate;
 	}
@@ -64,6 +76,14 @@ public class Dto {
 
 	public void setbHit(int bHit) {
 		this.bHit = bHit;
+	}
+
+	public int getbStep() {
+		return bStep;
+	}
+
+	public void setbStep(int bStep) {
+		this.bStep = bStep;
 	}
 
 	public int getbGroup() {
